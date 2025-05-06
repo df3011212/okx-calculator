bd188088e9f040921ce35daaec5dfc1b0f3807e6
df3011212 <df3011212@gmail.com>
2025-05-07 07:25:20 +0800

    Create specifiedAvg.js

---
+++ js/features/specifiedAvg.js
@@ -0,0 +1,8 @@
+// 指定均價計算功能 stub
+window.initSpecifiedAvg = function(){
+    document.getElementById("featureContent").innerHTML = `
+      <h4 style="color:#fff;text-align:center;">指定均價計算</h4>
+      <p style="padding:10px;color:#fff;">……自行實作……</p>
+    `;
+  };
+  
