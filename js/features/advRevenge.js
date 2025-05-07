f43ddba063dc3381ad279f72a201da7846394976
df3011212 <df3011212@gmail.com>
2025-05-07 08:33:22 +0800

    Add files via upload

---
+++ js/features/advRevenge.js
@@ -0,0 +1,8 @@
+// 高級反擊速算表功能 stub
+window.initAdvRevenge = function(){
+    document.getElementById("featureContent").innerHTML = `
+      <h4 style="color:#fff;text-align:center;">高級反擊速算表</h4>
+      <p style="padding:10px;color:#fff;">……自行實作……</p>
+    `;
+  };
+  
